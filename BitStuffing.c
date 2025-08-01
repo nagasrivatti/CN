3064063121e3df7a6199a6f3be916f5595c999c4
Siddireddi Shanmukha Sanjana <151708467+Sanjana-Siddireddi@users.noreply.github.com>
2025-08-01 12:07:49 +0530

    BitStuffing.c

---
+++ BitStuffing.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+void bitStuffing(char data[])
+{
+        char stuffed[100];
+        int i, j, count=0;
+        for(i=0;i<strlen(data);i++)
+        {
+                stuffed[j++]=data[i];
+                if(data[i]=='1')
+                {
+                        count++;
+                        if(count==5)
+                        {
+                                stuffed[j++]='0';
+                                count=0;
+                        }
+                }
+                else
+                {
+                        count=0;
+                }
+        }
+        stuffed[j]='\0';
+        printf("stuffed data: %s\n", stuffed);
+}
+void main()
+{
+        char data[50];
+        printf("Enter binary data(ex:1011110):");
+        scanf("%s", data);
+        bitStuffing(data);
+}
